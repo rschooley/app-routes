62e38b5a1e299a522387395c5484972e34fc5268
TJ Krusinski <tjkrus@gmail.com>
2013-09-05 15:46:01 -0400

    adds options object and tests
    can now pass in object as second parameter to change default locations for controllers
    
    Also has tests, run $ npm test

---
+++ tests/insta.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = function() {
+    return {    
+        index: function(req, res, next) {},
+        show: function(req, res, next) {},
+        create: function(req, res, next) {},
+        update: function(req, res, next) {},
+        destroy: function(req, res, next) {},
+    };
+};
